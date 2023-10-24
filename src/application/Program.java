9d4509e0447cc4fce0ef4ff2d5e9954e28647b14
Ranieri Pioner <rani_pioner@hotmail.com>
2023-10-23 22:31:12 -0300

    .gitignore updates

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,7 +40,7 @@ public class Program {
 				}
 				
 				if (chessMatch.getPromoted() != null) {
-					System.out.println("Enter piece for promotion(B/N/R/Q)");
+					System.out.println("Enter piece for promotion(B/N/R/Q):");
 					String type = sc.nextLine();
 					chessMatch.replacePromotedPiece(type);
 				}
